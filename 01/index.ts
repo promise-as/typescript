d3c2f3ddc6c438c41f1aa9a6197f1fa66e431fae
Hong Baojin <13809735212.163.com>
2020-04-08 18:42:45 +0800

    vscode watch typescript

---
+++ 01/index.ts
@@ -0,0 +1,18 @@
+console.log('你好ts')
+
+function getData() {
+  
+}
+
+var str:string = '你好ts';
+
+/*
+
+vscode 配置自动编译 ts
+
+  1. 第一步  tsc --init 生成 tsconfig.json  改 "outDir": "./js"
+
+  2. 第二步  Terminal -> Run Task -> tsc:watch - tsconfig.json, 
+    如果报错就运行系统自带的 cmd
+
+*/
---
+++ 01/js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+console.log('你好ts');
+function getData() {
+}
+var str = '你好ts';
+/*
+
+vscode 配置自动编译 ts
+
+  1. 第一步  tsc --init 生成 tsconfig.json  改 "outDir": "./js"
+
+  2. 第二步  Terminal -> Run Task -> tsc:watch - tsconfig.json,
+    如果报错就运行系统自带的 cmd
+
+*/ 
